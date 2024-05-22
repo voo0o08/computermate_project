a25e1cf29f6afa4490aa1958a4a29443e0e0bea3
voo0o08 <pdbstj050303@gmail.com>
2024-05-22 14:25:55 +0900

    n_temp_live
    노즐 데이터로 실시간 중계

--- CM_WEB/cm_web/views/main_views.py
+++ CM_WEB/cm_web/views/main_views.py
@@ -3,7 +3,6 @@ from flask import Blueprint, redirect, jsonify
 # Response, request => HTML 응답 요청을 처리하기 위함 
 # render_template => HTML 파일을 렌더링
 from flask import Flask, render_template, Response, request
-
 import pandas as pd 
 import json
 import plotly
@@ -11,6 +10,9 @@ import plotly.express as px
 import numpy as np
 
 bp = Blueprint("main", __name__, url_prefix="/")
+
+WINDOW_SIZE = 10
+cnt = 0
 
 # @=>데코레이터 
 @bp.route("/")
@@ -29,16 +31,21 @@ def index():
     
     return render_template("index.html", title="Home", graph1JSON=graph1JSON, graph2JSON=graph2JSON)
 
+
+# 차트는 여기서 업데이트됨
 @bp.route('/update_chart')
 def update_chart():
+    global WINDOW_SIZE
+    global cnt
     print("Updating chart...")  # 로그 추가 -> python terminal에서 확인
     # 데이터 업데이트 로직 (예시)
-    df = pd.DataFrame({
-        'x': [1, 2, 3],
-        'y': np.random.randint(1, 10, 3)  # 여기에 새로운 데이터로 변경
-    })
+    cm_data = pd.read_csv("./cm_web/static/data/for_web.csv")
     
-    new_fig = px.scatter(df, x="x", y="y")
+    col_data = cm_data["n_temp_pv"]
+    new_fig = px.line(x=range(WINDOW_SIZE), y=col_data[cnt:cnt+WINDOW_SIZE],
+                      markers=True)
+    new_fig.update_layout(yaxis_range=[60, 80])
+    
     new_graphJSON = json.dumps(new_fig, cls=plotly.utils.PlotlyJSONEncoder)
-    # return jsonify(graph1JSON)
+    cnt += 1 # WINDOW 이동
     return jsonify({"new_graphJSON": new_graphJSON, "msg" : "화이팅!"})

6ba2d17a38568fc568bd89664f9873c821b6c8b1
voo0o08 <pdbstj050303@gmail.com>
2024-05-26 20:10:38 +0900

    day0526

--- CM_WEB/cm_web/static/js/main.js
+++ CM_WEB/cm_web/static/js/main.js
@@ -1,6 +1,7 @@
 var $j = jQuery.noConflict(); // $j로 jQuery 객체 저장 -> 이거 없으면  충돌남
 console.log("jQuery type:", typeof $j);  // jQuery가 올바르게 로드되었는지 확인
 
+// $(document).ready 블록 내부에 있는 코드는 문서가 완전히 로드된 후 실행
 $j(document).ready(function() {
   var graph1DataElement = $j('#graph1-data');
   // 그래프 초기화 
@@ -8,6 +9,7 @@ $j(document).ready(function() {
     try {
       var graph1 = JSON.parse(graph1DataElement.text());
       Plotly.newPlot("chart1", graph1, {});  // 차트 생성
+      console.log("chart ready");
     } catch (e) {
       console.error("Error parsing JSON data:", e);
     }
@@ -15,22 +17,77 @@ $j(document).ready(function() {
     console.error("#graph1-data element not found");
   }
 
-  // 그래프 업데이트 
-  function updateChart() {
-    $j.getJSON('/update_chart', function(data) {
-      console.log("Data received:", data);  // 콘솔 로그 추가
-      console.log(data.msg);
-      
-      try {
-        var new_graph = JSON.parse(data.new_graphJSON);  // JSON 문자열을 객체로 변환
-        Plotly.react("chart1", new_graph.data, new_graph.layout);  // 차트 업데이트
-      } catch (e) {
-        console.error("Error parsing JSON data:", e);
-      }
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-      console.error("AJAX request failed:", textStatus, errorThrown);  // 에러 로그 추가
-    });
-  }
+  // // buttons = 버튼 전체 선택 
+  // let buttons = document.querySelectorAll('.updatemenu-item-rect');
+  // console.log(`======= buttons =======`)
+  // console.log(`${buttons}`)
+  // // 각 버튼에 대해 클릭 이벤트 리스너 추가
+  // buttons.forEach(button => {
+  //   button.onclick = function() {
+  //     console.log('안녕하세요!');
+  //   };
+  // });
+  
 
+// 그래프 업데이트 
+function updateChart(now_button) {
+  // update_chart 뭔지 잘 보기 여러가지 version이 있어욤
+  $j.getJSON('/update_chart', function(data) {
+    console.log("Data received:", data);  // 콘솔 로그 추가
+    console.log(data.msg);
+    
+    try {
+      var new_graph = JSON.parse(data.new_graphJSON);  // JSON 문자열을 객체로 변환
+      Plotly.react("chart1", new_graph.data, new_graph.layout);  // 차트 업데이트
+    } catch (e) {
+      console.error("Error parsing JSON data:", e);
+    }
+  }).fail(function(jqXHR, textStatus, errorThrown) {
+    console.error("AJAX request failed:", textStatus, errorThrown);  // 에러 로그 추가
+  });
+  }
   setInterval(updateChart, 500);
 });
+// ready 끝
+
+// let now_button = "E_scr" // 그래프 초기 값 설정 
+// // buttons = 버튼 전체 선택 
+// let buttons = document.querySelectorAll('.button_');
+
+// console.log(`======= buttons =======`)
+// console.log(`${buttons}`)
+// // 각 버튼에 대해 클릭 이벤트 리스너 추가
+// buttons.forEach(button => {
+//   button.onclick = function() {
+//     console.log(`Button text => ${this.textContent}`);
+//   };
+//   now_button = this.textContent
+
+document.addEventListener("DOMContentLoaded", function() {
+  // 버튼들을 가져옵니다.
+  let buttons = document.querySelectorAll('.button_');
+
+  // 각 버튼에 대해 클릭 이벤트 리스너를 추가합니다.
+  buttons.forEach(button => {
+    button.addEventListener("click", function() {
+      let buttonValue = this.value; // 클릭된 버튼의 값 가져오기
+
+      // 서버로 AJAX 요청을 보냅니다.
+      let xhr = new XMLHttpRequest();
+      xhr.open("POST", "/click_button"); // 요청을 보낼 URL을 지정합니다.
+      xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      xhr.onload = function() {
+        if (xhr.status == 200) {
+          console.log("Button value sent to server:", buttonValue);
+        } else {
+          console.error("Request failed:", xhr.status, xhr.statusText);
+        }
+      };
+      xhr.onerror = function() {
+        console.error("Network error");
+      };
+      xhr.send(JSON.stringify({ button: buttonValue })); // 버튼의 값을 JSON 형태로 서버에 전송합니다.
+    });
+  });
+});
+
--- CM_WEB/cm_web/views/main_views.py
+++ CM_WEB/cm_web/views/main_views.py
@@ -7,11 +7,28 @@ import pandas as pd
 import json
 import plotly
 import plotly.express as px
+import plotly.graph_objects as go
 import numpy as np
 
 bp = Blueprint("main", __name__, url_prefix="/")
 
 CM_DF = pd.read_csv("./cm_web/static/data/for_web.csv")
+
+E_scr_pv = CM_DF["E_scr_pv"]
+E_scr_sv = CM_DF["E_scr_sv"]
+
+c_temp_pv = CM_DF["c_temp_pv"]
+c_temp_sv = CM_DF["c_temp_sv"]
+
+k_rpm_pv = CM_DF["k_rpm_pv"]
+k_rpm_sv = CM_DF["k_rpm_sv"]
+
+n_temp_pv = CM_DF["n_temp_pv"]
+n_temp_sv = CM_DF["n_temp_sv"]
+
+s_temp_pv = CM_DF["s_temp_pv"]
+s_temp_sv = CM_DF["s_temp_sv"]
+
 WINDOW_SIZE = 30
 cnt = 0
 
@@ -22,21 +39,66 @@ def index():
 
 
 # dashboard =========================================================================
-@bp.route("/dash")
+@bp.route("/dash", methods=['POST'])
 def dash():
-    # graph one
-    df = px.data.medals_wide()
-    fig1 = px.bar(df, x="nation", y=["gold", "silver", "bronze"], title="WideForm Input")
     
-    graph1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
+    # # graph one
+    # df = px.data.medals_wide()
+    # fig1 = px.bar(df, x="nation", y=["gold", "silver", "bronze"], title="WideForm Input")
     
-    # Graph Two
-    df = px.data.iris()
-    fig2 = px.scatter_3d(df, x="sepal_length", y="sepal_width", z="petal_width",
-                         color="species", title="Iris")
-    graph2JSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
+    # graph1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
     
-    return render_template("dash.html", title="Home", graph1JSON=graph1JSON, graph2JSON=graph2JSON)
+    # # Graph Two
+    # df = px.data.iris()
+    # fig2 = px.scatter_3d(df, x="sepal_length", y="sepal_width", z="petal_width",
+    #                      color="species", title="Iris")
+    # graph2JSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    ###########################################################################################
+    global WINDOW_SIZE
+    global CM_DF
+    global cnt
+    print("그래프 초기화")  # 로그 추가 -> python terminal에서 확인
+    
+    #Figure 생성
+    new_fig = go.Figure()
+
+    #그래프 생성
+    x = ['A', 'B', 'C', 'D']
+
+    # 1번 그래프
+    new_fig.add_trace(go.Bar(name='Data 1',x=x, y=[100, 200, 500, 673]))
+
+    # 2번 그래프
+    new_fig.add_trace(go.Bar(name='Data 2',x=x, y=[56, 123, 982, 213]))
+
+    # 버튼 3개 생성
+    new_fig.update_layout(
+        updatemenus=[
+            dict(
+                type="buttons",
+                buttons=list([
+                    dict(label="Both",
+                        method="update",
+                        args=[{"visible": [True, True]},
+                            {"title": "Both"}]),
+                    dict(label="Data 1",
+                        method="update",
+                        args=[{"visible": [True, False]},
+                            {"title": "Data 1",}]),
+                    dict(label="Data 2",
+                        method="update",
+                        args=[{"visible": [False, True]},
+                            {"title": "Data 2",}]),
+                ]),
+            ),
+        ]
+    )
+    
+    new_graphJSON = json.dumps(new_fig, cls=plotly.utils.PlotlyJSONEncoder)
+    cnt += 1 # WINDOW 이동
+    
+    return render_template("dash.html", title="Home", graph1JSON=new_graphJSON)
 
 
 # 차트는 여기서 업데이트됨
@@ -45,7 +107,7 @@ def update_chart():
     global WINDOW_SIZE
     global CM_DF
     global cnt
-    print("Updating chart...")  # 로그 추가 -> python terminal에서 확인
+    print("Updating chart1...")  # 로그 추가 -> python terminal에서 확인
     # 데이터 업데이트 로직 (예시)
     
     col_data = CM_DF["n_temp_pv"]
@@ -57,7 +119,78 @@ def update_chart():
     cnt += 1 # WINDOW 이동
     return jsonify({"new_graphJSON": new_graphJSON, "msg" : "화이팅!"})
 
+
+# 차트는 여기서 업데이트됨
+@bp.route('/update_chart2')
+def update_chart2():
+    global WINDOW_SIZE
+    global CM_DF
+    global cnt
+    print("Updating chart2...")  # 로그 추가 -> python terminal에서 확인
+    # 데이터 업데이트 로직 (예시)
+    
+    # col_data = CM_DF["n_temp_pv"]
+    # new_fig = px.line(x=range(WINDOW_SIZE), y=col_data[cnt:cnt+WINDOW_SIZE],
+    #                   markers=True)
+    # new_fig.update_layout(yaxis_range=[60, 80])
+    #################################### button test code 
+    
+    #Figure 생성
+    new_fig = go.Figure()
+
+    #그래프 생성
+    x = ['A', 'B', 'C', 'D']
+
+    # 1번 그래프
+    new_fig.add_trace(go.Bar(name='Data 1',x=x, y=[100, 200, 500, 673]))
+
+    # 2번 그래프
+    new_fig.add_trace(go.Bar(name='Data 2',x=x, y=[56, 123, 982, 213]))
+
+
+    # 버튼 3개 생성
+    new_fig.update_layout(
+        updatemenus=[
+            dict(
+                type="buttons",
+                buttons=list([
+                    dict(label="Both",
+                        method="update",
+                        args=[{"visible": [True, True]},
+                            {"title": "Both"}]),
+                    dict(label="Data 1",
+                        method="update",
+                        args=[{"visible": [True, False]},
+                            {"title": "Data 1",}]),
+                    dict(label="Data 2",
+                        method="update",
+                        args=[{"visible": [False, True]},
+                            {"title": "Data 2",}]),
+                ]),
+            ),
+        ]
+    )
+    
+    new_graphJSON = json.dumps(new_fig, cls=plotly.utils.PlotlyJSONEncoder)
+    cnt += 1 # WINDOW 이동
+    return jsonify({"new_graphJSON": new_graphJSON, "msg" : "이것은 update2!"})
+
+
+
 # 1번 공장 =======================================================================================
 @bp.route('/factory1')
 def factory1():
     return render_template("factory.html")
+
+# button 값 확인 
+@bp.route('/click_button', methods=['POST'])
+def click_button():
+    data = request.get_json()
+    print(data)
+    # now_button = data.get('button')
+    # print(f"now_button: {now_button}")  # 터미널에 출력
+    
+    print("버튼이 클릭되었습니다!")
+    return jsonify({"message": "버튼이 클릭되었습니다!"})
+
+    # return jsonify(success=True)

a10df518e91d3a24b4b3e904013ead2a1aafa908
juugii-ho <77222382+juugii-ho@users.noreply.github.com>
2024-06-04 10:45:49 +0900

    현장 영상 추가

--- CM_WEB/cm_web/static/js/total.js
+++ CM_WEB/cm_web/static/js/total.js
@@ -3,3 +3,27 @@ document.addEventListener("DOMContentLoaded", function() {
     Plotly.newPlot('accumulate_graph', graphJSON.data, graphJSON.layout);
 });
 
+
+document.addEventListener("DOMContentLoaded", function() {
+    var video = document.getElementById('myVideo');
+
+    // 동영상 자동 재생 설정
+    video.muted = true; // muted 속성 설정
+    video.play(); // 동영상 재생
+
+    // 동영상 재생 중지
+    video.pause();
+
+    // 다른 동영상 이벤트 처리
+    video.addEventListener('play', function() {
+        console.log('동영상 재생 시작');
+    });
+
+    video.addEventListener('pause', function() {
+        console.log('동영상 일시 중지');
+    });
+
+    video.addEventListener('ended', function() {
+        console.log('동영상 종료');
+    });
+});
